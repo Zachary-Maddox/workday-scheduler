238f2cfcfe04bb4ffa3102d3f77c24cda9a45850
Zachary-Maddox <z19s96p@aol.com>
2022-07-02 22:06:25 -0500

    Added time block functionality ,  interval 5 mins

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+const saveBtnElArray = document.querySelectorAll(".saveBtn");
+const userInputEl = document.querySelectorAll(".userInput");
+var datePlaceEl = document.getElementById("datePlace");
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + "/" + dd + "/" + yyyy;
+document.getElementById("datePlace").innerHTML = today;
+
+function interfaceDisplay() {
+    var currentHour = moment().hour();
+    console.log(userInputEl);
+    userInputEl.forEach(function (textarea) {
+        console.log(textarea);
+        var dataHour = textarea.getAttribute("data-hour");
+        var dataHourNumber = parseInt(dataHour);
+        console.log(dataHourNumber);
+        if (dataHourNumber === currentHour) {
+            textarea.classList.remove("future");
+            textarea.classList.remove("past");
+            textarea.classList.add("present");
+        } else if (dataHourNumber < currentHour) {
+            textarea.classList.remove("future");
+            textarea.classList.remove("present");
+            textarea.classList.add("past");
+        } else {
+            textarea.classList.remove("past");
+            textarea.classList.remove("present");
+            textarea.classList.add("future");
+        }
+    });
+}
+setInterval(interfaceDisplay, 300000);
+interfaceDisplay();
+
+button.addEventListener("click");
